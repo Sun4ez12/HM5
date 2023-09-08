15ef9f635135e218325696095e41d45b3b478579
Sun4ez12 <flockivorkur@gmail.com>
2023-09-09 01:06:06 +0700

    Did my homework

---
+++ Task34/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+int CountElem(int[] arr)
+{
+    int res = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (Test(arr[i]))
+        {
+            res++;
+        }
+    }
+    return res;
+}
+
+bool Test(int n)
+{
+    return (n % 2 == 0);
+}
+
+int[] testArr = GenArray(10,100,999);
+
+PrintArr(testArr);
+
+int count = CountElem(testArr);
+
+PrintData("Количество четных элементов равно: "+count);
---
+++ Task36/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+
+int[] GenArray(int len, int minValue, int maxValue)
+{
+    int[] arr = new int[len];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return arr;
+}
+
+void PrinArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write(arr[i] + ", ");
+    }
+    Console.WriteLine(arr[arr.Length - 1] + "]");
+}
+
+void PrintData(string res)
+{
+    Console.WriteLine(res);
+}
+
+int SumPos (int [] arr)
+{
+    int res = 0;
+    for (int i = 1; i < arr.Length; i = i + 2)
+    {
+        res += arr[i];
+    }
+    return res;
+}
+
+int[] testArr = GenArray(10, 1, 10);
+PrinArr(testArr);
+int oddValueSum = SumPos(testArr);
+PrintData("Сумма нечетных элементов массива равна " + oddValueSum);
